ec3b4e2c909916cffc106cd526c569665c971c30
vism00 <xqreox@gmail.com>
2019-12-09 17:16:12 +0100

    ADD new java file

---
+++ src/test/java/cz/vse/selenium/Test_Suite_Creating_Project.java
@@ -0,0 +1,82 @@
+package cz.vse.selenium;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+
+/**
+ * Unit test for simple App.
+ */
+public class Test_Suite_Creating_Project {
+    private ChromeDriver driver;
+    private String url="https://digitalnizena.cz/rukovoditel/index.php?module=users/login";
+
+    @Before
+    public void init() throws IOException {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        ChromeOptions cho = new ChromeOptions();
+        cho.addArguments("--headless");
+        cho.addArguments("start-maximized");
+        cho.addArguments("window-size=1200,1100");
+        cho.addArguments("--disable-gpu");
+        cho.addArguments("--disable-extensions");
+        //driver = new ChromeDriver(cho);
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+//        driver.close();
+    }
+
+    @Test
+    public void login_first_test() {
+        driver.get(url);
+        WebElement searchInput = driver.findElement(By.name("username"));
+        searchInput.sendKeys("rukovoditel");
+        searchInput = driver.findElement(By.name("password"));
+        searchInput.sendKeys("vse456ru");
+        searchInput.sendKeys(Keys.ENTER);
+        Assert.assertTrue(driver.getTitle().startsWith("Rukovoditel | Dashboard"));
+        driver.quit();
+    }
+
+    @Test
+    public void login_second_test() {
+        driver.get(url);
+        WebElement searchInput = driver.findElement(By.name("username"));
+        searchInput.sendKeys("testNeprejde");
+        searchInput = driver.findElement(By.name("password"));
+        searchInput.sendKeys("vse456ru");
+        searchInput.sendKeys(Keys.ENTER);
+        Assert.assertTrue(!driver.getTitle().startsWith("Rukovoditel | Dashboard"));
+        driver.quit();
+    }
+
+    @Test
+    public void logout_third_test() {
+        driver.get(url);
+        WebElement searchInput = driver.findElement(By.name("username"));
+        searchInput.sendKeys("rukovoditel");
+        searchInput = driver.findElement(By.name("password"));
+        searchInput.sendKeys("vse456ru");
+        searchInput.sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector(".fa-angle-down")).click();
+        driver.findElement(By.cssSelector(".fa-angle-down")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Logoff")));
+        driver.findElement(By.linkText("Logoff")).click();
+        driver.quit();
+    }
+}
